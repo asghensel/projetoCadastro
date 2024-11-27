c0dd7d82e90d555c9888179f45f00b070cbaf099
asghensel <brunin6670@gmail.com>
2024-11-27 19:16:27 -0300

    27/11/24 19:16

--- controle/ativos_controle.php
+++ controle/ativos_controle.php
@@ -1,41 +1,41 @@
 <?php
 include('../modelo/conexao.php');
+include('controle_session.php');
+
+
 
 $descricao = $_POST['descricao'];
 $quantidade_ativo = $_POST['quantidade'];
 $marca_ativo= $_POST['marca'];
 $tipo_ativo = $_POST['tipo'];
 $observacao_ativo = $_POST['observacao'];
+$user=$_SESSION['id_user'];
+
 
 $query= "
-    insert into usuario (
+    insert into ativo (
                     descricaoAtivo,
                     quantidadeAtivo,
+                    statusAtivo,
                     idMarca,
-                    idTipo
+                    idTipo,
                     observacaoAtivo,  
-                    dataCadastro)values(
+                    dataCadastroAtivo,
+                    idUsuario)values(
                     '".$ativo."',
                     '".$quantidade_ativo."',
+                    'S',
                     '".$marca_ativo."',
                     '".$tipo_ativo."',
                     '".$observacao_ativo."',
-                    NOW()
+                    NOW(),
+                    '".$user."',
+                    
                                 )
 
         ";
 
 $result = mysqli_query($conexao, $query) or die(false);      
-if($result){
-    echo "<script> alert('usuario cadastrado')
-        window.location.href='../visao/login_usuario.php';
-    </script>";
-
-}else{
-    echo "<script> alert('Falha no Cadastro!')
-    window.location.href='../visao/cadastrar_usuario.php';
-    </script>";
-}
 
 
 ?>
--- controle/login_usuario_controle.php
+++ controle/login_usuario_controle.php
@@ -8,7 +8,8 @@ $usuario = $_POST['usuario'];
 $crip= base64_encode($senha);
 
 $sql="Select 
-            count(*) as quantidade 
+            count(*) as quantidade,
+            idUsuario
         from 
             usuario         
         where  
@@ -22,6 +23,7 @@ $dados = $result->fetch_assoc();
 if($dados['quantidade']>0){
     $_SESSION['login_ok']=true;
     $_SESSION['controle_login']=true;
+    $_SESSION['id_user']=$dados['idUsuario'];
     header('location:../visao/listar_usuario.php');
 }else{
     $_SESSION['login_ok']=false;
